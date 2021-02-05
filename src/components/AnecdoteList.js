cab0d29ec2ad48b7e65b262aef0203dadcd6b5fa
bryant tunbutr <bryantt23@users.noreply.github.com>
2021-02-04 19:30:22 -0800

    Cleanup

--- src/components/AnecdoteList.js
+++ src/components/AnecdoteList.js
@@ -1,20 +1,12 @@
 import React from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
 import { anecdoteVote } from '../reducers/anecdoteReducer';
 import { notificationAddVote } from '../reducers/notificationReducer';
 import { connect } from 'react-redux';
 
 function AnecdoteList(props) {
-  // const filter = useSelector(state => state.filter);
-  // console.log('filter', filter);
-  // const anecdotes = useSelector(state => state.anecdotes);
-  // const dispatch = useDispatch();
-
   const vote = anecdote => {
     props.anecdoteVote(anecdote);
     props.notificationAddVote(anecdote.content, 3);
-    // dispatch(anecdoteVote(anecdote));
-    // dispatch(notificationAddVote(anecdote.content, 3));
   };
 
   let sortedAnecdotes = props.anecdotes
